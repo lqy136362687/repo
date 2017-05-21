2aad2c94edd3022b147d9e5d61b3940d2d7a1196
2522824400@qq.com <2522824400@qq.com>
2017-05-21 23:03:48 +0800

    5

--- main/main.js
+++ main/main.js
@@ -1,18 +1,45 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maximum()}
+o) 元素数量 = ${sequence.lengthlong()}
+o) 平均值 = ${sequence.average()}`);
 };
 
 class Sequence {
   constructor(input) {
     // Write your code here
+    input=[6, 9, 15, -2, 92, 11];
+    var intIp=new Array();
+    for(let i=0;i<input.length;i++){
+      intIp[i]=input[i];
+    }
+    var mini=intIp[0];
+    var max=intIp[0];
+    var sum=0;
+    for(let i=0;i<input.length;i++){
+      mini=Math.min(intIp[i],mini);
+      max=Math.max(intIp[i],max);
+      sum+=intIp[i];
+    }
+    this.mini=mini;
+    this.maxi=max;
+    this.lengths=intIp.length;
+    this.aver=Math.round((sum/this.lengths)*100)/100;
   }
 
   minimum() {
     // Write your code here
+    return this.mini;
   }
-
-  // Write your code here
+  maximum() {
+    // Write your code here
+    return this.maxi;
+  }
+  lengthlong(){
+    return this.lengths;
+  }
+  average(){
+    return this.aver;
+  }
 }
