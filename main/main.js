37fb2b6dc9193aba492c15a0552c88a878834585
2522824400@qq.com <2522824400@qq.com>
2017-05-22 17:10:32 +0800

    work7

--- main/main.js
+++ main/main.js
@@ -1,3 +1,12 @@
 module.exports = function main(email, suffixes) {
   // Write your code here
+  var lastwords=email.split("@")[1];
+  var domain=lastwords.split(".")[0];
+  for(let i=0;i<suffixes.length;i++){
+    if(domain==suffixes[i].split(".")[0]){
+      return true;
+      break;
+    }
+  }
+  return false;
 };
